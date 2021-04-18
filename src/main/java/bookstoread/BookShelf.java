b060c24593fc3c04c22ff1ed0bfb60ccd99a0c1a
beginerbeginer <miyabi6125@gmail.com>
2021-04-18 10:14:35 +0900

    追加：メソッドについてのコメントアウト

--- src/main/java/bookstoread/BookShelf.java
+++ src/main/java/bookstoread/BookShelf.java
@@ -3,6 +3,10 @@ package bookstoread;
 import java.util.Collections;
 import java.util.List;
 
+/*テストケースを通過させるために、BookShelf books メソッドの実装を変更
+ *空のリストを返すようにした
+ * */
+
 public class BookShelf {
   public List<String> books() {
     return Collections.emptyList();
