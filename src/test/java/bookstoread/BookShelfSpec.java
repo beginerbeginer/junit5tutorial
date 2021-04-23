a94a17ba17583727ba564a7be9a8f66c759fc62e
beginerbeginer <miyabi6125@gmail.com>
2021-04-23 22:26:30 +0900

    Groovyでよく使われてテストメソッド名

--- src/test/java/bookstoread/BookShelfSpec.java
+++ src/test/java/bookstoread/BookShelfSpec.java
@@ -1,16 +1,16 @@
 package bookstoread;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 public class BookShelfSpec {
 
   //本棚を作成したときに本が入っていない（つまり空である）ことをテストする
   @Test
-  void shelfEmptyWhenNoBookAdded() throws Exception {
+  void shelf_empty_when_no_book_added() throws Exception {
     BookShelf shelf = new BookShelf();
     List<String> books = shelf.books();
     assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
