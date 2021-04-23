520cb3e0dadee1e283e4a96dd6dcb411fc06af58
beginerbeginer <miyabi6125@gmail.com>
2021-04-23 22:34:22 +0900

    編集：@DisplayNameアノテーションを使用して表示されるテストメソッドを変更

--- src/test/java/bookstoread/BookShelfSpec.java
+++ src/test/java/bookstoread/BookShelfSpec.java
@@ -4,13 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("A bookshelf")
 public class BookShelfSpec {
 
   //本棚を作成したときに本が入っていない（つまり空である）ことをテストする
   @Test
-  void shelf_empty_when_no_book_added() throws Exception {
+  @DisplayName("is empty when no book is added to it")
+  void shelfEmptyWhenNoBookAdded() throws Exception {
     BookShelf shelf = new BookShelf();
     List<String> books = shelf.books();
     assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
