1f6de9beb26251df25a24a8c6220da5830a88c6f
beginerbeginer <miyabi6125@gmail.com>
2021-04-23 21:34:45 +0900

    削除：public修飾子（JUnit5から可能な書式）

--- src/test/java/bookstoread/BookShelfSpec.java
+++ src/test/java/bookstoread/BookShelfSpec.java
@@ -10,7 +10,7 @@ public class BookShelfSpec {
 
   //本棚を作成したときに本が入っていない（つまり空である）ことをテストする
   @Test
-  public void shelfEmptyWhenNoBookAdded() throws Exception {
+  void shelfEmptyWhenNoBookAdded() throws Exception {
     BookShelf shelf = new BookShelf();
     List<String> books = shelf.books();
     assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
