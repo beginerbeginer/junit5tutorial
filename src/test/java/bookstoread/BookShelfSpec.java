2a3af2ca5eefa7b5f9835e75897ccdf40293b06d
beginerbeginer <miyabi6125@gmail.com>
2021-05-15 08:26:10 +0900

    修正：インデント

--- src/test/java/bookstoread/BookShelfSpec.java
+++ src/test/java/bookstoread/BookShelfSpec.java
@@ -15,44 +15,44 @@ import src.main.java.bookstoread.BookShelf;
 @DisplayName("A bookshelf")
 public class BookShelfSpec {
 
-	private BookShelf shelf;
+    private BookShelf shelf;
 
-	@BeforeEach
-	void init() throws Exception {
-		shelf = new BookShelf();
-	}
+    @BeforeEach
+    void init() throws Exception {
+        shelf = new BookShelf();
+    }
 
-  //本棚を作成したときに本が入っていない（つまり空である）ことをテストする
-  @Test
-  @DisplayName("is empty when no book is added to it")
-  void shelfEmptyWhenNoBookAdded() throws Exception {
-    List<String> books = shelf.books();
-    assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
-  }
+    // 本棚を作成したときに本が入っていない（つまり空である）ことをテストする
+    @Test
+    @DisplayName("is empty when no book is added to it")
+    void shelfEmptyWhenNoBookAdded() throws Exception {
+        List<String> books = shelf.books();
+        assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
+    }
 
-  @Test
-  public void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
-    shelf.add("Effective Java", "Code Complete");
-    List<String> books = shelf.books();
-    assertEquals(2, books.size(), () -> "BookShelf should have two books.");
-  }
+    @Test
+    public void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
+        shelf.add("Effective Java", "Code Complete");
+        List<String> books = shelf.books();
+        assertEquals(2, books.size(), () -> "BookShelf should have two books.");
+    }
 
-  @Test
-  public void emptyBookShelfWhenAddIsCalledWithoutBooks() {
-    shelf.add();
-    List<String> books = shelf.books();
-    assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
-  }
+    @Test
+    public void emptyBookShelfWhenAddIsCalledWithoutBooks() {
+        shelf.add();
+        List<String> books = shelf.books();
+        assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
+    }
 
-  @Test
-  void booksReturnedFromBookShelfIsImmutableForClient() {
-  	shelf.add("Effective Java", "Code Complete");
-  	List<String> books = shelf.books();
-  	try {
-  		books.add("The Mythical Man-Month");
-  		fail(() -> "Should not be able to add book to books");
-		} catch (Exception e) {
-  		assertTrue(e instanceof UnsupportedOperationException, () -> "Should throw UnsupportedOperationException");
-  	}
-  }
+    @Test
+    void booksReturnedFromBookShelfIsImmutableForClient() {
+        shelf.add("Effective Java", "Code Complete");
+        List<String> books = shelf.books();
+        try {
+            books.add("The Mythical Man-Month");
+            fail(() -> "Should not be able to add book to books");
+        } catch (Exception e) {
+            assertTrue(e instanceof UnsupportedOperationException, () -> "Should throw UnsupportedOperationException");
+        }
+    }
 }
