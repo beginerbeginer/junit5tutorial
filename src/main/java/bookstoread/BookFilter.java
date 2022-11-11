fe688ac205eebac2cc8e2038bff16aa23789ae35
beginerbeginer <miyabi6125@gmail.com>
2022-11-12 05:51:52 +0900

    【終了】80p テスト成功、追加：本の検索機能のテスト

--- src/main/java/bookstoread/BookFilter.java
+++ src/main/java/bookstoread/BookFilter.java
@@ -1,2 +1,33 @@
-package bookstoread;public interface BookFilter {
+package bookstoread;
+
+import java.time.LocalDate;
+
+public interface BookFilter {
+  boolean apply(Book book);
+}
+
+class BookPublishedYearFilter implements BookFilter {
+  /**
+   * startDate 開始日
+   */
+  private LocalDate startDate;
+  /**
+   * 引数yearよりも後に出版された本か判定する
+   * @param year 年
+   * @param month 月
+   * @param day 日
+   */
+  static BookPublishedYearFilter After(int year) {
+    BookPublishedYearFilter filter = new BookPublishedYearFilter();
+    filter.startDate = LocalDate.of(year, 12, 31);
+    return filter;
+  }
+
+  @Override
+  public boolean apply(final Book book) { //finalの理由：本の情報を変更することはないため
+    /**
+     * @return 本の出版日がstartDateよりも後の場合true
+     */
+    return book.getPublishedOn().isAfter(startDate);
+  }
 }
--- src/test/java/bookstoread/BookFilterSpec.java
+++ src/test/java/bookstoread/BookFilterSpec.java
@@ -1,2 +1,35 @@
-package bookstoread;public class BookFilterSpec {
+package bookstoread;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BookFilterSpec {
+    private Book cleanCode;
+    private Book codeComplete;
+
+    @BeforeEach
+    void init() {
+        cleanCode = new Book("Clean Code", "Robert C. Martin", LocalDate.of(2008, Month.AUGUST, 1));
+        codeComplete = new Book("Code Complete", "Steve McConnel", LocalDate.of(2004, Month.JUNE, 9));
+    }
+
+    @Nested
+    @DisplayName("book published date")
+    class BookPublishedFilterSpec{
+        @Test
+        @DisplayName("is after specified year")
+        void validateBookPublishedDatePostAskedYear(){
+            BookFilter filter = BookPublishedYearFilter.After(2007);
+            assertTrue(filter.apply(cleanCode));
+            assertFalse(filter.apply(codeComplete));
+        }
+    }
 }
