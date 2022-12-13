3e9fa8c27a3cfb62da4f85d43d034c25d3c38817
beginerbeginer <miyabi6125@gmail.com>
2022-12-13 18:13:32 +0900

    【終了】86p テスト成功、追加：Mockオブジェクトを使用してフィルターの正常な処理（フィルターの呼び出し、フィルターによる入力の処理、期待結果を返す）を確認

--- src/test/java/bookstoread/BookFilterSpec.java
+++ src/test/java/bookstoread/BookFilterSpec.java
@@ -9,8 +9,20 @@ import org.mockito.Mockito;
 import java.time.LocalDate;
 import java.time.Month;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * 各テストに対して共通の振る舞いが期待できるテスト
+ */
+interface FilterBoundaryTests {
+    BookFilter get();
+
+    @Test
+    @DisplayName("should not fail for null book.")
+    default void nullTest() { assertThat(get().apply(null)).isFalse(); }
+}
 
 //publicをつけず同じパッケージ外からアクセスできないようにする長所：テストのみに使うクラスなので、パッケージ外からアクセスできないことで、テストのみに使うクラスであることを明示できる
 class BookFilterSpec {
@@ -24,65 +36,78 @@ class BookFilterSpec {
     }
 
     @Nested
-    @DisplayName("book published date")
-    class BookPublishedFilterSpec{
+    @DisplayName("book published date post specified year")
+    class BookPublishedAfterFilterSpec implements FilterBoundaryTests {
+        BookFilter filter;
+
+        @BeforeEach
+        void init() {
+            filter = BookPublishedYearFilter.After(2007);
+        }
+
+        @Override
+        public BookFilter get() {
+            return filter;
+        }
+
         @Test
-        @DisplayName("is after specified year")
-        void validateBookPublishedDatePostAskedYear(){
-            BookFilter filter = BookPublishedYearFilter.After(2007);
+        @DisplayName("should give matching book")
+        void validateBookPublishedDatePostAskedYear() {
             assertTrue(filter.apply(cleanCode));
             assertFalse(filter.apply(codeComplete));
         }
+    }
 
-        /**
-         * 処理の説明：filter(2007年1月1日)はcleanCode(2008年8月1日)よりも後(isAfter)ではない→filterはcleanCodeより前である
-         */
+    @Nested
+    @DisplayName("book published date pre specified year")
+    class BookPublishedBeforeFilterSpec implements FilterBoundaryTests {
+
+        BookFilter filter;
+
+        @BeforeEach
+        void init() {
+            filter = BookPublishedYearFilter.Before(2007);
+        }
+
+        @Override
+        public BookFilter get() { return filter; }
+
         @Test
-        @DisplayName("is before specified year")
-        void validateBookPublishedDatePostPreAskedYear(){
-            BookFilter filter = BookPublishedYearFilter.Before(2007);
+        @DisplayName("should give matching book")
+        void validateBookPublishedDatePreAskedYear() {
             assertFalse(filter.apply(cleanCode));
             assertTrue(filter.apply(codeComplete));
         }
     }
 
-    /**
-     * 複合条件が正しく機能することを確認するために、複数のフィルターを使用する
-     */
     @Test
-    @DisplayName("Composite criteria is based on multiple filters") //意味：
+    @DisplayName("Composite criteria is based on multiple filters(複合条件が正しく機能することを確認するために、複数のフィルターを使用する)")
     void shouldFilterOnMultiplesCriteria(){
         CompositeFilter compositeFilter = new CompositeFilter();
         compositeFilter.addFilter(bookFilter -> false); //意味：addFilterメソッドに引数bookFilterを渡す。戻り値はfalse。”->”はラムダ式の記法。bookFilterは引数、falseは戻り値
         assertFalse(compositeFilter.apply(cleanCode)); //
     }
 
-    /**
-     * 複合条件は最初の失敗の後、起動しない。
-     */
     @Test
-    @DisplayName("Composite criteria does not invoke after first failure")
-    void shouldNotInvokeAfterFirstFailure(){
+    @DisplayName("Composite criteria  invokes all in case of failure(複合条件の適応が失敗し、全てのフィルターが実行されることを確認する)")
+    void shouldInvokeAllInFailure(){
         CompositeFilter compositeFilter = new CompositeFilter();
 
         BookFilter invokedMockedFilter = Mockito.mock(BookFilter.class);
         Mockito.when(invokedMockedFilter.apply(cleanCode)).thenReturn(false);
         compositeFilter.addFilter(invokedMockedFilter);
 
-        BookFilter notInvokedMockedFilter = Mockito.mock(BookFilter.class);
-        Mockito.when(notInvokedMockedFilter.apply(cleanCode)).thenReturn(true);
-        compositeFilter.addFilter(notInvokedMockedFilter);
+        BookFilter secondInvokedMockedFilter = Mockito.mock(BookFilter.class);
+        Mockito.when(secondInvokedMockedFilter.apply(cleanCode)).thenReturn(true);
+        compositeFilter.addFilter(secondInvokedMockedFilter);
 
         assertFalse(compositeFilter.apply(cleanCode));
         Mockito.verify(invokedMockedFilter).apply(cleanCode);
-        Mockito.verifyZeroInteractions(notInvokedMockedFilter);
+        Mockito.verify(secondInvokedMockedFilter).apply(cleanCode);
     }
 
-    /**
-     * 複合条件はすべてのフィルタを呼び出す
-     */
     @Test
-    @DisplayName("Composite criteria invokes all filters")
+    @DisplayName("Composite criteria invokes all filters(複合条件が成功し、全てのフィルターが実行されることを確認する)")
     void shouldInvokeAllFilters(){
         CompositeFilter compositeFilter = new CompositeFilter();
 
